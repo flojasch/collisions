b813a2a1ec4504b2e90987fa3c885667dbc982f1
flojasch <f.jasch@gmx.net>
2019-03-03 19:57:01 +0000

    brownian motion

--- sketch.js
+++ sketch.js
@@ -1,14 +1,24 @@
 let balls = [];
-let anzahl = 80;
+let anzahl = 200;
+let rad = 10;
 
 function setup() {
-  let canvas = createCanvas(800, 600);
-  canvas.position(20, 20);
+  let canvas = createCanvas(windowWidth, windowHeight);
+  // let canvas = createCanvas(800, 600);
+  // canvas.position(20, 20);
+  let y = 0;
+  let x = 0;
   for (let i = 0; i < anzahl; i++) {
-    let x = i * width / anzahl;
-    let y = i * height / anzahl;
-    balls.push(new Ball(x, y, 3, -3));
+    if (x < width - 2 * rad - 1) {
+      x += 2 * rad + 1;
+    } else {
+      x = 0;
+      y += 2 * rad + 1;
+    }
+    balls.push(new Ball(x, y, 10, 0, 5));
   }
+  balls.push(new Ball(width / 2, height / 2, 50, 0, 0, 0, 0, 200));
+  // balls.push(new Ball(width / 2 + 200, height / 2, 20, -3, 2, 0, 0, 200));
 }
 
 function draw() {
