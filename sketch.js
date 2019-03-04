b43cb2d2cd3ac2bc187a24a1e37d58ec73f1df25
Florian <f.jasch@gmx.net>
2019-03-04 18:30:03 +0100

    velocity disttibution

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let balls = [];
-let anzahl = 200;
+let anzahl = 400;
 let rad = 10;
+let n = [];
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
@@ -15,10 +16,14 @@ function setup() {
       x = 0;
       y += 2 * rad + 1;
     }
-    balls.push(new Ball(x, y, 10, 0, 5));
+    if (i == anzahl - 1) {
+      balls.push(new Ball(x, y, 10, 0.0000001, 5, 0, 0, 200));
+    } else {
+      balls.push(new Ball(x, y, 10, 0, 5));
+    }
   }
-  balls.push(new Ball(width / 2, height / 2, 50, 0, 0, 0, 0, 200));
-  // balls.push(new Ball(width / 2 + 200, height / 2, 20, -3, 2, 0, 0, 200));
+  // balls.push(new Ball(width / 2, height / 2, 50, 0, 0, 0, 0, 200));
+
 }
 
 function draw() {
@@ -28,10 +33,18 @@ function draw() {
       Ball.collision(balls[i], balls[j]);
     }
   }
+  for (let i = 0; i < 200; i++) {
+    n[i] = 0;
+  }
+
   for (ball of balls) {
     ball.checkEdges();
     ball.update();
+    n[floor(p5.Vector.mag(ball.v))] += 1;
     ball.show();
   }
-
+  for (let i = 0; i < 200; i++) {
+    fill(250);
+    rect(i * 20, height - n[i] * 5, 20, n[i] * 5);
+  }
 }
